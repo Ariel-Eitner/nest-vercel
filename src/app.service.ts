643baa554a91a3e323bf76d5e051e3cf504b2bc4
Ariel-Eitner-MSK <arieleitner@msklatam.com>
2025-11-03 01:26:55 -0300

    test

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,17 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'backend iniciado exitosamente';
+    const start = Date.now();
+
+    // Cálculo pesado: sumar los primeros 500 millones
+    let sum = 0;
+    for (let i = 0; i < 500_000_000; i++) {
+      sum += i;
+    }
+
+    const end = Date.now();
+    const time = (end - start) / 1000; // segundos
+
+    return `Hola mundo! Cálculo terminado en ${time} segundos. Resultado parcial: ${sum}`;
   }
 }
